81d883e57cd047239e5048fd63b4f0387a541cea
Paul Mejia Feliz <paulmejiafeliz@gmail.com>
2024-11-19 20:28:23 -0500

    feat: Add translation example

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,25 @@
 import { FC } from 'react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { Card, Tag } from '../components';
 import { useUserContext } from '../hooks';
 
 import './Profile.scss';
 
 export const Profile: FC = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const { firstName, lastName, summary, skills } = useUserContext();
 
   return (
     <>
-      <h1>
-        {firstName} {lastName}
-      </h1>
+      <Card>
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        {t('helloWorld')}
+      </Card>
       <Card>
         <p>{summary}</p>
+
         <ul className="skills">
           {skills.map((s) => (
             <li key={s}>
